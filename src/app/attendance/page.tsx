7e7d189fc09e23796262fc8fdcc8cdb03f29bfb7
Sahadat Hossen <sahadatjh@gmail.com>
2026-02-16 11:20:39 +0600

    create attendance page

---
+++ src/app/attendance/page.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from "../../components/layout/MainLayout";
+
+export default function AttendancePage() {
+  return (
+    <MainLayout pageTitle="Overview">
+         Attendance Page
+    </MainLayout>
+  );
+}
