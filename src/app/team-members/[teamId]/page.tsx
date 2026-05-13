916d566fa2307934564ddb646872a289106e9beb
FyslAhmd <info.faysal.32@gmail.com>
2026-05-13 16:10:48 +0600

    feat: add AddTeamMemberModal component and integrate it into TeamMembersHeader for adding team members

--- src/app/team-members/[teamId]/page.tsx
+++ src/app/team-members/[teamId]/page.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import { MainLayout } from "@/components/layout/MainLayout";
 import { TeamMembersHeader } from "@/components/team/TeamMembersHeader";
 import { TeamMembersTable } from "@/components/team/TeamMembersTable";
+import { useParams } from "next/navigation";
 
 export default function TeamMembersDetailsPage() {
+  const params = useParams();
+  const teamId = params?.teamId as string;
+
   return (
     <MainLayout pageTitle="Team Members">
-      <TeamMembersHeader />
+      <TeamMembersHeader teamId={teamId} />
       <TeamMembersTable />
     </MainLayout>
   );
