4234cbdd9db0186e2434f08ad19357a7b5f1facb
FyslAhmd <info.faysal.32@gmail.com>
2026-05-16 11:27:16 +0600

    Refactor code structure for improved readability and maintainability

--- src/api/services/ProjectManagementService.ts
+++ src/api/services/ProjectManagementService.ts
@@ -21,6 +21,7 @@ import type { ProjectUpdateSuccessDto } from '../models/ProjectUpdateSuccessDto'
 import type { UpdateClientDto } from '../models/UpdateClientDto';
 import type { UpdateProfileDto } from '../models/UpdateProfileDto';
 import type { UpdateProjectDto } from '../models/UpdateProjectDto';
+import type { UserAssignedProjectSuccessDto } from '../models/UserAssignedProjectSuccessDto';
 import type { CancelablePromise } from '../core/CancelablePromise';
 import { OpenAPI } from '../core/OpenAPI';
 import { request as __request } from '../core/request';
@@ -435,7 +436,7 @@ export class ProjectManagementService {
          * Bearer token
          */
         authorization: string,
-    }): CancelablePromise<any> {
+    }): CancelablePromise<UserAssignedProjectSuccessDto> {
         return __request(OpenAPI, {
             method: 'GET',
             url: '/api/project/my-projects/{userId}',
